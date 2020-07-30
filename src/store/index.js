6e44351d4bb37724fb12e65514a79f5660860862
tonytiratayoclock <tony@oclock.io>
2020-07-30 16:09:11 +0200

    Use redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,21 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import reducer from "./reducer";
 // import logMiddleware from "./middlewares/logMiddleware";
 import ajaxMiddleware from "./middlewares/ajaxMiddleware";
 
-const store = createStore(reducer, applyMiddleware(ajaxMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancers = composeEnhancers(
+  applyMiddleware(
+    ajaxMiddleware
+    // secondMiddleware,
+  )
+);
+
+const store = createStore(
+  reducer,
+  // preloadedState,
+  enhancers
+);
 
 export default store;
