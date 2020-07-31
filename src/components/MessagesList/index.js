2b00f5d30a903487af304b32af24f21e9f9fc6be
tonytiratayoclock <tony@oclock.io>
2020-07-31 15:35:44 +0200

    Smooth auto scroll

--- src/components/MessagesList/index.js
+++ src/components/MessagesList/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './style.scss';
 
-const MessageList = ({ list, user }) => (
-  <div className="message-list">
-    {list.map((messageObject) => <Message key={messageObject.id} {...messageObject} connectedUser={user} />)}
-  </div>
-);
+const MessageList = ({ list, user }) => {
+  const containerElement = useRef(null);
 
+  useEffect(() => {
+    const scrollY = containerElement.current.scrollHeight;
+    containerElement.current.scrollTo(0, scrollY);
+  }, [list]);
+  return (
+    <div ref={containerElement} className="message-list">
+      {list.map((messageObject) => <Message key={messageObject.id} {...messageObject} connectedUser={user} />)}
+    </div>
+  );
+};
 const Message = ({ author, content, connectedUser }) => (
   <div className={connectedUser === author ? 'message' : 'message right'}>
     <div className="message_author">{author}</div>
