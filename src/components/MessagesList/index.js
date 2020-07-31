a1bf1ec93f099556d753db6f7b00f4f2032e14e6
Tony Tiratay <45205102+tonytiratayoclock@users.noreply.github.com>
2020-07-31 15:57:38 +0200

    Update index.js

--- src/components/MessagesList/index.js
+++ src/components/MessagesList/index.js
@@ -3,12 +3,21 @@ import PropTypes from 'prop-types';
 import './style.scss';
 
 const MessageList = ({ list, user }) => {
+  // Ici je prépare une variable qui contiendra une référence à la div qui liste les messages dans le dom.
+  // Comme elle n'existe pas encore, je donne null comme valeur. Et dans le render, j'informe react que je souhaite
+  // mettre la div qui liste les messages en référence dans cette variable
   const containerElement = useRef(null);
-
+  
+  // A chaque fois que la liste change, j'exécute cette fonction
   useEffect(() => {
+    // Je récupère la hauteur de la div de messages (containerElement.current)
     const scrollY = containerElement.current.scrollHeight;
+    // Je la scrolle verticalement
     containerElement.current.scrollTo(0, scrollY);
   }, [list]);
+  
+  // Je mets une ref sur la div qui liste des messages, pour pouvoir y accéder dans la variable
+  // container element
   return (
     <div ref={containerElement} className="message-list">
       {list.map((messageObject) => <Message key={messageObject.id} {...messageObject} connectedUser={user} />)}
